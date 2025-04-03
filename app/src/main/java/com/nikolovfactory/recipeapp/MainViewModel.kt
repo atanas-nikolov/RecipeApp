13e288c39f4eee3dd0a63168ecf1ff027d0c1e50
atanas-nikolov <master89q@gmail.com>
2025-04-03 15:02:34 +0300

    add MainViewModel class

--- app/src/main/java/com/nikolovfactory/recipeapp/MainViewModel.kt
+++ app/src/main/java/com/nikolovfactory/recipeapp/MainViewModel.kt
@@ -1,4 +1,51 @@
 package com.nikolovfactory.recipeapp
 
-class MainViewModel {
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+class MainViewModel : ViewModel() {
+
+    // we are creating a private global variable _
+    private val  _categorieState = mutableStateOf(RecipeState())
+    // public variable we will be exposed to other classes
+    val  categoriesState: State<RecipeState> = _categorieState
+
+    init {
+        fetchCategories()
+    }
+
+
+    private fun fetchCategories(){
+        //CoroutineScope is allowed us to run routine/code on background
+        viewModelScope.launch {
+            try {
+                val  response = recipeService.getCategories()
+                _categorieState.value = _categorieState.value.copy(
+                    list = response.categories,
+                    loading = false,
+                    error = null
+                )
+
+
+
+            } catch (e: Exception) {
+                _categorieState.value = _categorieState.value.copy(
+                    loading = false,
+                    error = "Error fetching Categories ${e.message}"
+                )
+            }
+        }
+    }
+
+    data class RecipeState(
+        val loading: Boolean = true,
+        val list: List<Category> = emptyList(),
+        val error: String? = null
+
+    )
+
 }
